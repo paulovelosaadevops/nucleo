c222fe1a19dfac54c9f1d17d52f80526bbce6fde
paulovelossadevops <paulo.velosa@icloud.com>
2026-08-18 16:13:31 -0300

    chore: bootstrap Spring Boot API and PostgreSQL

---
+++ apps/api/src/main/java/br/com/nucleo/api/NucleoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nucleo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucleoApiApplication.class, args);
+	}
+
+}
---
+++ apps/api/src/test/java/br/com/nucleo/api/NucleoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nucleo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NucleoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
